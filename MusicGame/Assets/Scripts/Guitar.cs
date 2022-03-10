dc806e4868cd63b823be0c138a3751e224d1cd03
BillieEyelash <riatalwar@hotmail.com>
2022-03-09 21:10:28 -0800

    Guitar scene that plays chords

---
+++ MusicGame/Assets/Scripts/Guitar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Guitar : MonoBehaviour
+{
+    public Dropdown Dropdown;
+    public List<AudioSource> Chords;
+    private Color OriginalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OriginalColor = GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayChord()
+    {
+        AudioSource Chord = Chords[Dropdown.value - 1];
+        Chord.Play();
+    }
+}
