6cd0991a7fbcca934de49baeb040d8e066a483c8
Bill Baxter <bbaxteriv@Bills-MBP.lakesideschool.org>
2022-04-08 08:42:05 -0700

    tmp package and multiplayer package fix

--- MusicGame/Assets/Scripts/MultiplayerScripts/UIManager.cs
+++ MusicGame/Assets/Scripts/MultiplayerScripts/UIManager.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Netcode;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Button startServerButton;
     [SerializeField] private Button startHostButton;
     [SerializeField] private Button startClientButton;
-    [SerializeField] private TextMeshProUGUI playersInGameText;
+    [SerializeField] private Text playersInGameText;
 
     // Start is called before the first frame update
     private void Awake()
@@ -20,46 +20,46 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        //playersInGameText.text = $"Players in game: {PlayersManager.Instance.PlayersInGame}";
+        playersInGameText.text = $"Players in game: {PlayersManager.Instance.PlayersInGame}";
     }
     
     private void Start()
     {
-        /*
+        
         startHostButton.onClick.AddListener(() =>
         {
             if (NetworkManager.Singleton.StartHost())
             {
-                Logger.Instance.LogInfo("Host started...");
+                Debug.Log("Host started...");
             }
             else
             {
-
+                Debug.Log("Host could not be started...");
             }
         });
         startServerButton.onClick.AddListener(() =>
         {
             if (NetworkManager.Singleton.StartServer())
             {
-
+                Debug.Log("Server started...");
             }
             else
             {
-
+                Debug.Log("Server could not be started...");
             }
         });
         startClientButton.onClick.AddListener(() =>
         {
             if (NetworkManager.Singleton.StartClient())
             {
-
+                Debug.Log("Client started...");
             }
             else
             {
-
+                Debug.Log("Client could not be started...");
             }
         });
-        */
+        
     }
     
 }
