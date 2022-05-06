445216414af0252a386524733332c9ee9318ca2f
BillieEyelash <riatalwar@hotmail.com>
2022-05-06 08:42:11 -0700

    Simplify drum scripts

--- MusicGame/Assets/Scripts/Drum.cs
+++ MusicGame/Assets/Scripts/Drum.cs
@@ -10,22 +10,6 @@ public class Drum : Note
         base.Touchscreen();
     }
 
-    public void Touchscreen()
-    {
-        foreach (Touch touch in Input.touches)
-        {
-            if (touch.phase == TouchPhase.Began)
-            {
-                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
-                if (hit) // touchscreen
-                {
-                    hit.collider.gameObject.GetComponent<Note>().PlayNote();
-                }
-            }
-        }
-    }
-
-
     // Play note and change color when clicked
     void OnMouseDown()
     {
