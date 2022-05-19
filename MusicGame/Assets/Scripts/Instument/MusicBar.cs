c1d0948c8b36e97bdb2141aebe5a3ec4ca8eef12
Bill Baxter <bbaxteriv@Bills-MBP.lakesideschool.org>
2022-05-19 09:41:39 -0700

    working workspace box

--- MusicGame/Assets/Scripts/Instument/MusicBar.cs
+++ MusicGame/Assets/Scripts/Instument/MusicBar.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
+
+//BY DEFAULT: SPEED = 2; SONG LENGTH = 14
+
+
+
 public class MusicBar : MonoBehaviour
 {
     private Rigidbody2D rb;
@@ -11,6 +18,8 @@ public class MusicBar : MonoBehaviour
     public float speed;
     public Text lengthText;
     public float length;
+
+    private float snapBackPosition;
 
     private void Awake()
     {
@@ -21,22 +30,37 @@ public class MusicBar : MonoBehaviour
     private void FixedUpdate()
     {
         speed = 2f;
+
+        length = 14f;
+
+        snapBackPosition = length - 1f;
+
         // Get input speed
-        int number;
-        if (int.TryParse(speedText.text.ToString(), out number))
+        int number1;
+        if (int.TryParse(speedText.text.ToString(), out number1))
         {
-            speed = number;
+            speed = number1;
         }
+        // Get input length
+        
+        int number2;
+        if (int.TryParse(lengthText.text.ToString(), out number2))
+        {
+            length = number2;
+        }
+
+        snapBackPosition = length - 1f;
+
         // Set max speed
         if (speed > 20f)
         {
             speed = 20f;
         }
-        
+
         pos = transform.position;
         rb.velocity = new Vector2(speed, 0f);
         // Go back to beginning of grid after reaching the end
-        if (pos[0] > 13f)
+        if (pos[0] > snapBackPosition)
         {
             transform.position = new Vector3(-1f, 3.99f, 0f);
         }
--- MusicGame/Assets/Scripts/Track+Record/WorkspaceScript.cs
+++ MusicGame/Assets/Scripts/Track+Record/WorkspaceScript.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class WorkspaceScript : MonoBehaviour
 {
-
-    //pls no delete
+    public GameObject bar;
+    private float songLength;
+    private Vector3 storedLength;
+    private Vector3 storedPosition = new Vector3(6.5f, 3.99f, 0f);
+    private float xPos = 6f;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+        storedLength = transform.localScale;
+        storedLength.x = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lTemp = storedLength;
+        songLength = bar.GetComponent<MusicBar>().length;
+
+        lTemp.x *= songLength;
+
+        transform.localScale = lTemp;
+
+        xPos = ((songLength - 1f) / 2f);
+
+        storedPosition = new Vector3(xPos, 3.99f, 0f);
+
+        transform.localPosition = storedPosition;
     }
 }
+
+
+
+
