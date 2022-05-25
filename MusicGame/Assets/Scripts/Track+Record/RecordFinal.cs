7eb7e9beb22153a805cd6cb63212265892736cc8
Calvin <calvin.tran14@rspic.org>
2022-05-24 23:50:56 -0700

    Connecting Wav files to server

--- MusicGame/Assets/Scripts/Track+Record/RecordFinal.cs
+++ MusicGame/Assets/Scripts/Track+Record/RecordFinal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -9,6 +10,7 @@ public class RecordFinal : MonoBehaviour
 {
     private AudioRenderer Renderer;
     public Button RecButton;
+    public string username = "calvin";
     private int clickNumber = 0;
 
     // Start is called before the first frame update
@@ -31,7 +33,7 @@ public class RecordFinal : MonoBehaviour
                 var path = AssetDatabase.GUIDToAssetPath(asset);
                 AssetDatabase.DeleteAsset(path);
             }
-            
+
             Renderer.Rendering = true;
             RecButton.GetComponent<Image>().color = new Color(166f/255f, 0, 0);
         }
@@ -44,9 +46,22 @@ public class RecordFinal : MonoBehaviour
 
     public void EndRecording()
     {
-        Renderer.Save("./Assets/Resources/Exports/export.wav"); // unique identifier on these files with game id?
+        Renderer.Save("./Assets/Resources/Exports/export.wav");
         AssetDatabase.Refresh();
         Renderer.Rendering = false;
         SceneManager.LoadScene("Preview");
+
+        //connecting to website
+        string url = "https://1920.lakeside-cs.org/MultiplayerMusicGame/MusicGame/insert.php?";
+        StartCoroutine(fillForm(url));
+    }
+
+    IEnumerator fillForm(string url)
+    {
+       WWWForm form = new WWWForm();
+       form.AddBinaryData("fileToUpload", File.ReadAllBytes("./Assets/Resources/Exports/export.wav"), "audio/wav");
+       form.AddField("username", username);
+       WWW www = new WWW(url, form);
+       yield return www;
     }
 }
